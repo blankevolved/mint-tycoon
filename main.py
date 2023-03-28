cf645f3ca793f1b2da832f47cf2ff0649c23d9e3
Blank <69070442+blankminecraft@users.noreply.github.com>
2023-03-28 11:18:07 -0500

    update1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Market():
     def __init__(self):
         self.coins_val = {}
         for c in coin_list:
-            self.coins_val[c] = coin_list.get(c).start_val
+            self.coins_val[c] = coin_list.get(c).min_val
 
     def refresh_vals(self):
         for c in coin_list:
@@ -23,15 +23,15 @@ class Market():
 coin_list = {}
 
 class Coin():
-    def __init__(self, name:str, min_val:float, max_val:float, start_val:float):
+    def __init__(self, name:str, min_val:float, max_val:float):
         self.name = name.lower() + '_coin'
         self.display_name = name.capitalize() + ' Coin'
         self.min_val = min_val
         self.max_val = max_val
-        self.start_val = start_val
         coin_list[self.name] = self
     def __repr__(self):
         return self.name + '_object'
+
 
 
 class Tycoon():
@@ -44,14 +44,7 @@ class Tycoon():
             self.produces[c] = 0
             self.coins[c] = 0
         self.upgrades = {}
-    def create_upgrade(self, name:str, display_name:str, cost:float, coin_cost:Coin, boost:float, boost_coin:Coin):
-        self.upgrades[name] = {
-            'display_name':display_name,
-            'coin_cost':coin_cost,
-            'cost':cost,
-            'boost':boost,
-            'boost_coin':boost_coin,
-        }
+
     def buy_upgrade(self, name:str):
         name = name.lower().replace(' ', '_')
         try:
@@ -59,14 +52,64 @@ class Tycoon():
         except:
             print(f"{name} isn't a valid upgrade")
             return
+        if upgrade['has'] == True:
+            print(f"{name} isn't a valid upgrade")
+            return
         coin_amt = self.coins[upgrade['coin_cost'].name]
-        if coin_amt == upgrade['cost']:
+        if coin_amt >= upgrade['cost']:
             self.coins[upgrade['coin_cost'].name] = self.coins[upgrade['coin_cost'].name] - upgrade['cost']
             self.produces[upgrade['boost_coin'].name] = self.produces[upgrade['boost_coin'].name] + upgrade['boost']
-            self.upgrades.pop(name)
+            upgrade['has'] = True
 
-bronze_coin = Coin('Bronze', .1, 1, .5)
-silver_coin = Coin('Silver', 3, 10, 3.75)
+    def list_upgrades(self):
+        for u in self.upgrades:
+            if tycoon.upgrades[u]['needs'] == () and self.upgrades[u]['has'] == False:
+                print(f"({u}) {self.upgrades[u]['display_name']} costs {self.upgrades[u]['cost']} {self.upgrades[u]['coin_cost'].display_name}'s, makes {self.upgrades[u]['boost']} {self.upgrades[u]['boost_coin'].display_name}'s p/s")
+            
+            for nu in self.upgrades[u]['needs']:
+
+                if self.upgrades[nu].get('has') == True:
+                    print(f"({u}) {self.upgrades[u]['display_name']} costs {self.upgrades[u]['cost']} {self.upgrades[u]['coin_cost'].display_name}'s, makes {self.upgrades[u]['boost']} {self.upgrades[u]['boost_coin'].display_name}'s p/s")
+
+    def list_produced(self):
+        print('Produced:')
+        for index, c in enumerate(self.coins, 1):
+            if index % 5 == 0:
+                print(f"[{self.coins[c]} {coin_list[c].display_name}'s]", end='\n')
+            else:
+                print(f"[{self.coins[c]} {coin_list[c].display_name}'s]", end=' ')
+
+        print(f"[{self.bux} Bux's]", end=' ')
+        
+
+        print('\n')
+    
+    def list_produces(self):
+        print('Produces:')
+        for p in self.produces:
+            print(f"[{self.produces[p]} {coin_list[p].display_name}'s p/s]", end=' ')
+        print('\n')
+
+class Upgrade():
+    def __init__(self, tycoon:Tycoon, name:str, display_name:str, cost:float, coin_cost:Coin, boost:float, boost_coin:Coin, *needed_before_available):
+        self.tycoon = tycoon
+        self.tycoon.upgrades[name] = {
+            'display_name':display_name,
+            'coin_cost':coin_cost,
+            'cost':cost,
+            'boost':boost,
+            'boost_coin':boost_coin,
+            'has':False,
+            'needs':needed_before_available
+        }
+
+    
+
+bronze_coin = Coin('Bronze', .1, 1)
+silver_coin = Coin('Silver', 3, 5)
+gold_coin = Coin('Gold', 7, 12)
+diamond_coin = Coin('Diamond', 9, 23)
+
 
 tycoon = Tycoon('player')
 
@@ -74,55 +117,85 @@ tycoon = Tycoon('player')
 market = Market()
 
 
-tycoon.create_upgrade('copper_machine_1', 'Copper Machine 1', 0, bronze_coin, 1, bronze_coin)
+copper_1 = Upgrade(tycoon, 'copper_machine_1', 'Copper Machine 1', 0, bronze_coin, 1, bronze_coin)
+copper_2 = Upgrade(tycoon, 'copper_machine_2', 'Copper Machine 2', 50, bronze_coin, 1, bronze_coin, 'copper_machine_1')
+
 
 
 while __name__ == '__main__':
     time1 = time()
-    def tycoon_menu():
-        time1 = time()
-        print(f"{tycoon.owner}'s Tycoon:")
-        print('\nProduces:')
-        for p in tycoon.produces:
-            print(f"{tycoon.produces[p]} {coin_list[p].display_name}'s p/s")
-        print('\nProduced:')
-        for c in tycoon.coins:
-            print(f"{tycoon.coins[c]} {coin_list[c].display_name}'s")
-        print('\n1. view available upgrades')
-        inp = input('>>> ')
-        if inp == '1':
-            clear()
-            for u in tycoon.upgrades:
-                print(f"({u}) {tycoon.upgrades[u]['display_name']} costs {tycoon.upgrades[u]['cost']} {tycoon.upgrades[u]['coin_cost'].display_name}'s, makes {tycoon.upgrades[u]['boost']} {tycoon.upgrades[u]['boost_coin'].display_name}'s p/s")
-            print('\nuse "buy", to buy a upgrade (buy some_upgrade_name)\n')
-            inp = input('>>> ')
-            for u in list(tycoon.upgrades):
-                if 'buy' in inp and u in inp:
-                    tycoon.buy_upgrade(u)
-            clear()
-        else:
-            clear()
-        time2 = time()
+
+    def give_coins(time1, time2):
         for c in coin_list:
             tycoon.coins[c] = round(tycoon.coins[c] + tycoon.produces[c] * round(time2 - time1, 1), 1)
 
+    def tycoon_menu():
+        time1 = time()
+
+        print(f"{tycoon.owner.capitalize()}'s Tycoon:")
+
+        tycoon.list_produces()
+
+        tycoon.list_produced()
+
+        print('1. view available upgrades')
+
+        inp = input('>>> ')
+
+        time2 = time()
+        give_coins(time1, time2)
+        
+        if inp == '1':
+            clear()
+
+
+            tycoon.list_upgrades()
+
+
+            print('\nType the name of an upgrade to buy it (some_upgrade_name)\n')
+
+            inp = input('>>> ')
+
+            if inp in list(tycoon.upgrades):
+                tycoon.buy_upgrade(inp)
+                clear()
+                tycoon.list_upgrades()
+
+
+                print('\nType the name of an upgrade to buy it (some_upgrade_name)\n')
+            
+            clear()
+            
+        elif inp == 'back':
+            clear()
+        else:
+            clear()
+            tycoon_menu()
+
     def market_menu():
         time1 = time()
+
         print('Market Values: ')
         market.get_vals()
+
         inp = input('>>> ')
+
         if inp == '1':
             clear()
             print(1)
         else:
             clear()
+
         time2 = time()
-        for c in coin_list:
-            tycoon.coins[c] = round(tycoon.coins[c] + tycoon.produces[c] * round(time2 - time1, 1), 1)
+        give_coins(time1, time2)
 
     print("""Pick a Menu:
 1. Tycoon
 2. Market""")
+    
+    time2 = time()
+    give_coins(time1, time2)
+
     inp = input('>>> ')
     if inp == '1':
         clear()
@@ -132,9 +205,3 @@ while __name__ == '__main__':
         market_menu()
     else:
         clear()
-
-    time2 = time()
-
-    print(tycoon.coins)
-    for c in coin_list:
-        tycoon.coins[c] = round(tycoon.coins[c] + tycoon.produces[c] * round(time2 - time1, 1), 1)
